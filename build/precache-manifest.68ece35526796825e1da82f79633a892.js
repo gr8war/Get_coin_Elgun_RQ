6d7665c8445e6f6d21f54b437801534edd910e27
gr8war <mrdobrak248@gmail.com>
2023-01-03 23:57:11 +0400

    first commit

---
+++ build/precache-manifest.68ece35526796825e1da82f79633a892.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7210eab3312e25fb92b7365fe819f51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f896e7a1be34d54890a",
+    "url": "/static/css/main.c4c7950c.chunk.css"
+  },
+  {
+    "revision": "3d9f946b712c09ac1501",
+    "url": "/static/js/2.c9727d21.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c9727d21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f896e7a1be34d54890a",
+    "url": "/static/js/main.d2f81845.chunk.js"
+  },
+  {
+    "revision": "f50eec9e4568160fa8df",
+    "url": "/static/js/runtime-main.a0845817.js"
+  },
+  {
+    "revision": "a886eaffb543cb46de50ae2b3253a8be",
+    "url": "/static/media/arrow_down.a886eaff.svg"
+  },
+  {
+    "revision": "eee1fbd8d27dd51b6f1ab8be22e21d4a",
+    "url": "/static/media/arrow_up.eee1fbd8.svg"
+  },
+  {
+    "revision": "c89fc995807b90f21e53c8b09bc12729",
+    "url": "/static/media/coin.c89fc995.svg"
+  },
+  {
+    "revision": "9ecb58e5cfb6175081e92c6039b67f1e",
+    "url": "/static/media/eye.9ecb58e5.svg"
+  }
+]);
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/Clown';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { BrowserRouter } from "react-router-dom";
+import rootReducer from './store/reducers';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <BrowserRouter> <Provider store={store}> <App /> </Provider> </BrowserRouter>,
+  document.getElementById('root')
+);
